d35b82caf6a60bd2d083b9c8591dae8514755403
ghostwriter <ghostwriternr@gmail.com>
2016-03-14 18:53:14 +0530

    Cleaner version of frontend added
    All instances of hall name removed.

---
+++ Frontend/src/frontend/Frontend.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frontend;
+
+/**
+ *
+ * @author ghost_000
+ */
+public class Frontend {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        home hm = new home();
+        hm.setVisible(true);
+    }
+    
+}
